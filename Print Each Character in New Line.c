f62009ba4d544842d54d330d899a1be7d7bd4541
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-07-20 21:16:32 +0530

    Add program to print each character in a new line

---
+++ Print Each Character in New Line.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+
+    return 0;
+}
