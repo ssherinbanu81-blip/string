fd714e7733879547576e8ac8a9bbfd73eae89a36
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-07-20 21:15:56 +0530

    Add Toggle Case program in C

---
+++ Toggle Case.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = str[i] + 32;
+
+        else if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+
+        i++;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
