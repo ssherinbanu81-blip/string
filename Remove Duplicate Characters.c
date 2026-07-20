840c18d745697bbdd7424b5e3dac9fac496096ce
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-07-20 21:20:47 +0530

    Add program to remove duplicate characters from string

---
+++ Remove Duplicate Characters.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j;
+
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+
+        for (j = i + 1; str[j] != '\0'; j++) {
+
+            if (str[i] == str[j]) {
+
+                int k;
+
+                for (k = j; str[k] != '\0'; k++)
+                    str[k] = str[k + 1];
+
+                j--;
+            }
+        }
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
