c9a2346721b7f4275e656dc7bb413cc89088fef7
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-07-20 21:19:44 +0530

    Add word counting program in Count Number of Words.c
    This program counts the number of words in a given string by counting spaces.

---
+++ Count Number of Words.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, words = 1;
+
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+
+        if (str[i] == ' ')
+            words++;
+
+        i++;
+    }
+
+    printf("%d", words);
+
+    return 0;
+}
