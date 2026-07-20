0845da70a456ba6fcd9ed426f596394fcf363f90
S.Sherin Banu <ssherinbanu81@gmail.com>
2026-07-20 21:14:55 +0530

    Add program to convert uppercase to lowercase

---
+++ Convert Uppercase to Lowercase.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = str[i] + 32;
+
+        i++;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
